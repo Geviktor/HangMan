e268a9e2f4b555337c65f80ea2b6c3fcdf900687
qualder <qualder@protonmail.com>
2020-04-15 11:25:27 +0300

    Update

--- hangmantest.py
+++ hangmantest.py
@@ -1,4 +1,4 @@
-#Modules
+#modules
 from os import system
 from random import randint
 
@@ -9,7 +9,9 @@ def printWord(letters):
 
 
 def checkWord(word_letters,gues_letters,gues,chance):
-    list1 = gues_letters
+    list1 = []
+    for i in gues_letters:
+        list1.append(i)
 
     for indx, letter in enumerate(word_letters):
         if letter == gues:
@@ -17,45 +19,64 @@ def checkWord(word_letters,gues_letters,gues,chance):
     
     if list1 == gues_letters:
         chance -= 1
-
     return gues_letters, chance
 
 
 def selectWord():
-    x = randint(1,5)
-    if x == 1:
-        return "masa"
-    elif x == 2:
-        return "kağıt"
-    elif x == 3:
-        return "demir"
-    elif x == 4:
-        return "kulaklık"
-    elif x == 5:
-        return "bilgisayar"
+    words = ["kulaklık","masa","bilgisayar","demir","kağıt","kalem"]
+    x = randint(0,5)
+    return words[x]
 
+def gameOver(gues_letters,chance):
+    checker = 0
+    for i in gues_letters:
+        if i == "~":
+            checker += 1
 
-def gameOver(chance):
     if chance == 0:
+        return False
+    elif checker == 0:
         return False
     else:
         return True
 
-def main():
+def singlePlayer():
     word = selectWord()
     chance = len(word)
     print(chance)
 
     word_letters = list(word)
     gues_letters = ["~"]*len(word)
+    the_letters = []
 
-    while gameOver(chance):
+    while gameOver(gues_letters,chance):
         system("clear")
+        print(f"Your Chance: {chance}     The letters you entered: {the_letters} \n")
         printWord(gues_letters)
-        print(chance)
-        gues = input("Enter your gues: ")
+        gues = input("\nEnter your gues: ")
 
-        gues_letters, chance = checkWord(word_letters,gues_letters,gues,chance)
+        try:
+            gues = int(gues)
+            input("Letter must be string! Press Enter.")
+        except ValueError:
+            if len(gues) > 1:
+                input("Letter must be 1 character! Press Enter.")
+            else:
+                gues = gues.lower()
+                if gues in the_letters:
+                    input("You used this letter! Press enter.")
+                else:
+                    the_letters.append(gues)
+                    gues_letters, chance = checkWord(word_letters,gues_letters,gues,chance)
+
+    if chance == 0:
+        print(f"Game Over! The Word: {word}")
+    else:
+        print(f"You Win! The Word: {word}")
+        
+
+def main():
+    singlePlayer()
 
 
 if __name__ == "__main__":
